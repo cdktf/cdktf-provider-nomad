f3e03314630876c89614a4f554f103251a269a05
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2022-11-03 00:05:07 +0000

    chore(deps): upgrade dependencies
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/cdktf/cdktf-provider-nomad/actions/runs/3382023852
    
    ------
    
    *Automatically created by projen via the "upgrade-main" workflow*
    
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- scripts/check-for-upgrades.js
+++ scripts/check-for-upgrades.js
@@ -11,7 +11,7 @@ const actions = require("@actions/core");
 
 // set by the projen file that generates this script
 const CONSTRAINT = "~> 1.4";
-const FQ_PROVIDER_NAME = "nomad";
+const FQ_PROVIDER_NAME = "hashicorp/nomad";
 
 (async function main() {
   if (await newerMatchingProviderVersionExists()) {
