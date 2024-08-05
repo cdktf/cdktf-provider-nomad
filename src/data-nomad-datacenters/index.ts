a31a754ac6c7595480168eef6a851175aeda89ab
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-05 11:16:42 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@3b3b11b

--- src/data-nomad-datacenters/index.ts
+++ src/data-nomad-datacenters/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/nomad/2.3.0/docs/data-sources/datacenters
 // generated from terraform resource schema
 
