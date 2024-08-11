9cb78a3965581fff2c7d79fae90194b716b59d24
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-11 22:07:56 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@2e1259a

--- src/job/index.ts
+++ src/job/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/nomad/2.3.0/docs/resources/job
 // generated from terraform resource schema
 
