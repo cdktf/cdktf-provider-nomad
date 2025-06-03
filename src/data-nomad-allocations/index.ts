db665831025d42bdd111cb75a7b6f998bdc38d61
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 14:52:21 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@4d35a18

--- src/data-nomad-allocations/index.ts
+++ src/data-nomad-allocations/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/nomad/2.5.0/docs/data-sources/allocations
 // generated from terraform resource schema
 
